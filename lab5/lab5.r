9002c8c6d770b775b66ade94f77fa98d2be890ef
Aleksandr Lapiy <alekslapiy@gmail.com>
2019-11-20 17:07:50 +0300

    fixes

--- lab5/lab5.r
+++ lab5/lab5.r
@@ -4,28 +4,30 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       sliderInput("x1",
-                  "Значение x1:",
+                  "Значение [1, 1]:",
                   min = 0,
                   max = 5,
                   value = 1,
                   step = 1),
       sliderInput("x2",
-                  "Значение x2:",
+                  "Значение [2, 2]:",
                   min = 0,
                   max = 5,
                   value = 1,
                   step = 1         
       ),
-      sliderInput("mu",
-                  "Значение μ:",
+      sliderInput("x3",
+                  "Значение [1, 2], [2, 1]:",
                   min = 0,
                   max = 5,
                   value = 0,
-                  step = 1),
-      
+                  step = 1)
     ),
     mainPanel(
       HTML("<center><h1><b>График линий уровня нормального распределения</b></h1>"),
+      textOutput(outputId = "sigmaMess1"),
+      textOutput(outputId = "sigmaMess2"),
+      h4(textOutput(outputId = "detSigma")),
       textOutput(outputId = "error"),
       plotOutput("plot")
     )
@@ -37,13 +39,26 @@ server <- function(input, output) {
   output$plot <- renderPlot({
     x1 <- input$x1
     x2 <- input$x2
-    mu <- input$mu
-    sigma <- matrix(c(x1, 0, 0, x2), 2, 2)
-    View(sigma)
+    x3 <- input$x3
+    mu <- matrix(0, 1, 2) # центр 0, 0
+    sigma <- matrix(c(x1, x3, x3, x2), 2, 2)
+    
+    output$sigmaMess1 = renderText({
+      paste(sigma[1,1],sigma[1,2],sep=" ")
+    })
+    
+    output$sigmaMess2 = renderText({
+      paste(sigma[2,1],sigma[2,2],sep=" ")
+    })
+    
+    output$detSigma = renderText({
+      paste("Определитель ", det(sigma))
+    })
+    
     if (det(sigma) <= 0) {
-      output$error <- renderText({"Определитель меньше или равен 0"})
+      output$error <- renderText({"Определитель меньше или равен 0, кривая второго порядка параболического или гиперболического типа"})
     } else {
-      output$error <- renderText({""})
+      output$error <- renderText({"Определитель больше 0, кривая второго порядка эллиптического типа"})
       
       normDistr = function(x1, x2, mu, sigma) {
         x <- matrix(c(x1, x2), 1, 2)
